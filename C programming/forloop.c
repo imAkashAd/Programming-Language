6f4b0f704ef6bf2ed20c373b6f551a4bf9b6cb95
AK <you@example.com>
2025-03-11 00:16:42 +0600

    for loop

---
+++ C programming/forloop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=0; i<=5; i++){
+        printf("%d Hey loop%d\n", i, i);
+    }
+    return 0;
+}
--- C programming/switch.c
+++ C programming/switch.c
@@ -1,32 +1,14 @@
-#include<stdio.h>
-int main(){
-    int day;
-    printf("Enter day number: ");
-    scanf("%d", &day);
-    switch (day)
+#include <stdio.h>
+    int main()
     {
-    case 1:
-        printf("Saturday.");
-        break;
-    case 2:
-        printf("Sunday.");
-        break;
-    case 3:
-        printf("Monday.");
-        break;
-    case 4:
-        printf("Tuesday.");
-        break;
-    case 5:
-        printf("Wednesday.");
-        break;
-    case 6:
-        printf("Thursday.");
-        break;
-    case 7:
-        printf("Friday.");
-        break;
-    default:
-        printf("Wrong input");
+        int x = 5;
+        if (x < 10) {
+            printf("hello ");
+        }
+        if (x == 5) {
+            printf("hi");
+        }
+        else {
+            printf("no");
+        }
     }
-}
