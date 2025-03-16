bda77a998e94a5c6ba3be13e92b4b02d2ad5e7f0
AK <akadhikary199901@gmail.com>
2025-03-16 21:13:32 +0600

    solving another problems

--- Python/My problem solving/problem5.py
+++ Python/My problem solving/problem5.py
@@ -1,1 +1,13 @@
 # Write a program that will reverse a four digit number. Also it checks whether the reverse is true(Palindrome).
+
+input = int(input("Enter a 4 digit number: "))
+
+if input >= 1000 and input <= 9999 :
+    reverse_number = int(str(input)[::-1])
+    print(f"The reverse number is : {reverse_number}")
+
+    if input==reverse_number:
+        print("It is a palindrom number.")
+
+else:
+    print("Wrong input.")
