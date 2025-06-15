c13cd93cbf3a6d47a0194cf49aa17fd92ba7568c
Rowshown Nahar Roshni <kaurritika791@gmail.com>
2025-06-16 00:15:11 +0600

    Creating Note app

---
+++ note_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Note_App());
+}
+
+class Note_App extends StatelessWidget {
+  const Note_App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Note App',
+      theme: ThemeData(primarySwatch: Colors.blueGrey ),
+    home: const,
+    );
+  }
+}
